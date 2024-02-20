de515c2dbed351d10dd804aa911afb22d5a6f454
Juankjos <juanca_jos@live.com.mx>
2024-02-19 19:19:18 -0600

    First commit

---
+++ src/main/java/com/qa/juan/App.java
@@ -0,0 +1,13 @@
+package com.qa.juan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
